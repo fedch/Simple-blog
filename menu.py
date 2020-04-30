6d38d860c6eba32891c805d6d0385a834f7fd7b2
Elena Fedchenko <fedchenko.yelena@gmail.com>
2020-04-30 18:56:07 +1200

    Added an option to delete a blog, fixed formating

--- menu.py
+++ menu.py
@@ -1,10 +1,11 @@
 from database import Database
 from models.blog import Blog
+import pymongo
 
 class Menu(object): # extends from object
     def __init__(self):
         # Ask the user for author name
-        self.user = input("What's your login? ")
+        self.user = input("Hi! What's your login? ")
         # Make an instance of user_blog, to which blog will equal to if exists:
         self.user_blog = None
         if self._user_has_account():
@@ -36,28 +37,40 @@ class Menu(object): # extends from object
 
     def run_menu(self):
         # User read or write blogs?
-        read_or_write = input("Are you here to read or write? R/W: ")
-        if read_or_write == "R":
+        action = input("Are you here to read, write or delete an entry? R/W/D: ")
+        if action == "R" or action == "r":
             # List blogs in Database:
             self._list_blogs()
             # Allow the user to pick one and display posts:
             self._view_blog()
 
-        elif read_or_write == "W":
+        elif action == "W" or action == "w":
             # Prompt to write a post:
             self.user_blog.new_post()
+
+        elif action == "D" or action == "d":
+            # List blogs in Database:
+            self._list_blogs()
+            # ALlow user to delete en entry:
+            self._delete_blog()
+
         else:
             print("Thanks and bye!")
+
+    def _delete_blog(self):
+        blog_to_delete = input("Copy and paste the blog ID to delete it: ")
+        Database.delete_one(collection='blogs', query={'id': blog_to_delete})
+        print(f"Blog {blog_to_delete} deleted!")
 
     def _list_blogs(self):
         blogs = Database.find(collection='blogs',
                             query={})
         for blog in blogs:
-            print("ID: {}, Title: {}, Author: {}".format(blog['id'], blog['title'], blog['author']))
+            print(f"ID: {blog['id']}, Title: {blog['title']}, Author: {blog['author']}")
 
     def _view_blog(self):
         blog_to_see = input("Copy and paste the blog ID to read the blog: ")
         blog = Blog.from_mongo(blog_to_see)
         posts = blog.get_posts()
         for post in posts:
-            print("Date: {}, Title: {}\n\n{}".format(post['date'], post['title'], post['content']))
+            print(f"Date: {post['created_date']}, Title: {post['title']}\n\n{post['content']}")
