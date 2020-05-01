9d42583b87bd54e968b097ec31f8ae9276556934
Elena Fedchenko <fedchenko.yelena@gmail.com>
2020-05-01 16:41:18 +1200

    Added an option to delete an entry from own blog

--- menu.py
+++ menu.py
@@ -5,11 +5,11 @@ import pymongo
 class Menu(object): # extends from object
     def __init__(self):
         # Ask the user for author name
-        self.user = input("Hi! What's your login? ")
+        self.user = input("\nHi! What's your login? ")
         # Make an instance of user_blog, to which blog will equal to if exists:
         self.user_blog = None
         if self._user_has_account():
-            print (f"Welcome back, {self.user}")
+            print (f"\nWelcome back, {self.user}")
         else:
             self._prompt_user_for_account()
 
@@ -19,15 +19,15 @@ class Menu(object): # extends from object
         blog = Database.find_one(collection='blogs', query={'author': self.user})
         if blog is not None:
             # Assign an object of type blog to user blog
-            self.user_blog = Blog.from_mongo(blog['id'])
+            self.user_blog = Blog.from_mongo_id(blog['id'])
             return True
         else:
             return False
 
     # If user has no account yet, prompt them to create one:
     def _prompt_user_for_account(self):
-        title = input("Enter blog title: ")
-        description = input("Enter blog description: ")
+        title = input("\nEnter blog title: ")
+        description = input("\nEnter blog description: ")
         blog = Blog(author=self.user,
                     title=title,
                     description=description)
@@ -37,7 +37,11 @@ class Menu(object): # extends from object
 
     def run_menu(self):
         # User read or write blogs?
-        action = input("Are you here to read, write or delete an entry? R/W/D: ")
+        action = input("""\nWhat would you like to do today?\n\nChoose one of the following options:
+        Read a blog entrees (R)
+        Write a post (W)
+        Delete a blog (DB)
+        Delete an entree from your blog (DE)\n""")
         if action == "R" or action == "r":
             # List blogs in Database:
             self._list_blogs()
@@ -48,29 +52,45 @@ class Menu(object): # extends from object
             # Prompt to write a post:
             self.user_blog.new_post()
 
-        elif action == "D" or action == "d":
+        elif action == "DB" or action == "db":
             # List blogs in Database:
             self._list_blogs()
-            # ALlow user to delete en entry:
+            # ALlow user to delete a blog:
             self._delete_blog()
 
+        elif action == "DE" or action == "de":
+            # Let the user to delete an entree:
+            self._delete_entree()
+
         else:
-            print("Thanks and bye!")
+            print("\nThanks and bye!\n")
 
     def _delete_blog(self):
-        blog_to_delete = input("Copy and paste the blog ID to delete it: ")
+        blog_to_delete = input("\nCopy and paste the blog ID to delete it: ")
         Database.delete_one(collection='blogs', query={'id': blog_to_delete})
-        print(f"Blog {blog_to_delete} deleted!")
+        print(f"\nBlog {blog_to_delete} deleted!\n")
+
+    def _delete_entree(self):
+        blog = Blog.from_mongo_author(self.user)
+        # Show all posts
+        posts = blog.get_posts()
+        print("\nHere are all your posts: ")
+        for post in posts:
+            print(f"\nID: {post['id']}, Date: {post['created_date']}, Title: {post['title']}\n\n{post['content']}\n")
+        entree_to_delete = input("\nCopy and paste the ID of an entree to delete it: ")
+        Database.delete_one(collection='posts', query={'id': entree_to_delete})
+        print(f"\nEntree {entree_to_delete} deleted!\n")
 
     def _list_blogs(self):
         blogs = Database.find(collection='blogs',
                             query={})
         for blog in blogs:
-            print(f"ID: {blog['id']}, Title: {blog['title']}, Author: {blog['author']}")
+            print(f"\nID: {blog['id']}, Title: {blog['title']}, Author: {blog['author']}")
+            # Blog ID: {blog['blog_id']},
 
     def _view_blog(self):
-        blog_to_see = input("Copy and paste the blog ID to read the blog: ")
-        blog = Blog.from_mongo(blog_to_see)
+        blog_to_see = input("\nCopy and paste the blog ID to read the blog: ")
+        blog = Blog.from_mongo_id(blog_to_see) # found blog object
         posts = blog.get_posts()
         for post in posts:
-            print(f"Date: {post['created_date']}, Title: {post['title']}\n\n{post['content']}")
+            print(f"\nID: {post['id']}, Date: {post['created_date']}, Title: {post['title']}\n\n{post['content']}\n")
